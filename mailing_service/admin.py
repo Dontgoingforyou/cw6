03f59df145cd4388be043dd9294e35e8084d06a8
Dontgoingforyou <mishael000@yandex.ru>
2024-09-30 19:45:37 +0300

    the technical task has been implemented

---
+++ mailing_service/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from mailing_service.models import Client, Message, Mailing, Attempt
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment')
+    search_fields = ('email', 'full_name')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('topic',)
+    search_fields = ('topic',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start_datetime', 'periodicity', 'status', 'message')
+    list_filter = ('periodicity', 'status')
+    filter_horizontal = ('clients',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mailing', 'attempt_datetime', 'status')
+    list_filter = ('status',)
+    search_fields = ('mailing__id',)
